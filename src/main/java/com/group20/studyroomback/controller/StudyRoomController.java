920d5e38f08eecf8e6f8272f58c26bafab7bc01a
yanghong <18018520976@163.com>
2022-05-14 23:34:22 +0800

    modify StudyRoomController

--- src/main/java/com/group20/studyroomback/controller/StudyRoomController.java
+++ src/main/java/com/group20/studyroomback/controller/StudyRoomController.java
@@ -38,7 +38,7 @@ public class StudyRoomController {
     }
 
 
-    @PostMapping("/")
+    @PutMapping("/")
     public ResponseEntity<Response> updateStudyRoom(@RequestBody StudyRoom studyRoom){
         MultiValueMap<String, String> headers = new HttpHeaders();
         Response<StudyRoom> response = new Response<>();
@@ -56,7 +56,7 @@ public class StudyRoomController {
         return new ResponseEntity(response,headers,200);
     }
 
-    @PostMapping("/")
+    @PostMapping("")
     public ResponseEntity<Response> insertStudyRoom(@RequestBody StudyRoom studyRoom){
         MultiValueMap<String, String> headers = new HttpHeaders();
         int success_num = studyRoomService.insertStudyRoom(studyRoom);
